518fa9e0078b1ba989863e6b4d9b5a72fb9af890
cheonyeonsu <cheonyeonsu@gmail.com>
2024-05-17 17:23:41 +0900

    javascript day2

---
+++ event/event/js/event-listener.js
@@ -0,0 +1,13 @@
+function checkUsername(){
+    let elMsg = document.getElementById('feedback');
+    if(elUsername.value.length <5){
+        elMsg.textContent = '이름은 다섯글자 이상으로 입력하세요';
+    }else{
+        elMsg.textContent = '';
+    }
+}
+
+let elUsername = document.getElementById('username');
+elUsername.addEventListener('blur', checkUername, false);
+                            //blur가 일어나면 checkUername호출
+//html은 html이고 js는 js.                             
